5edc292d8db166bd287aae0b74040ae7564c2fa7
Vlad <vladmunj@gmail.com>
2023-03-15 19:18:00 +0600

    swagger make update

--- src/Console/SwaggerMakeCommand.php
+++ src/Console/SwaggerMakeCommand.php
@@ -39,9 +39,5 @@ class SwaggerMakeCommand extends Command
     {
         $process = Process::fromShellCommandline(base_path().'/vendor/bin/openapi app -o ./public/swagger.json');
         $process->run();
-        if(!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-        dump($process->getOutput());
     }
 }
