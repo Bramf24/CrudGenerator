020b3dd1213e419298596cd18b67e7a8e2a6e359
Vlad <vladmunj@gmail.com>
2023-03-16 14:50:54 +0600

    router builder update

--- src/Builders/Router.php
+++ src/Builders/Router.php
@@ -1,19 +1,25 @@
 <?php namespace Bramf\CrudGenerator\Builders;
 
 use Illuminate\Support\Facades\File;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Illuminate\Support\Facades\DB;
 
 class Router{
     public function __construct(
         private array $params
     ){
         $this->routesPath = base_path().'/vendor/bramf/crud-generator/src/routes/crud.php';
+        $this->output = new ConsoleOutput();
     }
 
     /**
      * Build routes file
      */
     public function build(){
-        // $routes = app()->router->getRoutes();
-        // dump($routes);
+        DB::table('crud_route_groups')->upsert([
+            ['name'=>$this->params['crud_url']]
+        ],['name']);
+        // $groups = DB::table('crud_route_groups')->get();
+
     }
 }
