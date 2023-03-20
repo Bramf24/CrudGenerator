f8f86de2d84dc5df6a1d45020a237e4413be3651
Vlad <vladmunj@gmail.com>
2023-03-20 10:18:23 +0600

    routes generation update

--- src/CrudGeneratorServiceProvider.php
+++ src/CrudGeneratorServiceProvider.php
@@ -98,7 +98,7 @@ class CrudGeneratorServiceProvider extends ServiceProvider{
     public function boot()
     {
         if(!file_exists(base_path().'/routes/crud/routes.php')){
-            $file = file_open(base_path().'/routes/crud/routes.php','w');
+            $file = fopen(base_path().'/routes/crud/routes.php','w');
             fclose($file);
         }
         $groupOptions = ['namespace' => 'App\Http\Controllers'];
