73421ab689cf25ca83c4b953fcda02d68b2ee8d8
Vlad <vladmunj@gmail.com>
2023-04-19 14:13:57 +0600

    rest action trait update

--- src/Traits/RestActions.php
+++ src/Traits/RestActions.php
@@ -1,7 +1,6 @@
 <?php namespace Bramf\CrudGenerator\Traits;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\JsonResponse;
 
 /**
  * trait for all REST actions
@@ -9,28 +8,28 @@ use Illuminate\Http\JsonResponse;
  * model must have public static $rules array to validate input data
  */
 trait RestActions{    
-    public function all(): JsonResponse{
+    public function all(): mixed{
         return response()->json(self::MODEL::all(),200);
     }
 
-    public function get(int $id): JsonResponse{
+    public function get(int $id): mixed{
         return response()->json(self::MODEL::find($id),200);
     }
 
-    public function create(Request $request): JsonResponse{
+    public function create(Request $request): mixed{
         $this->validate($request,self::MODEL::$rules);
         if(self::MODEL::where($request->all())->exists()) return false;
         return response()->json(self::MODEL::create($request->all()),201);
     }
 
-    public function update(Request $request, int $id): JsonResponse{
+    public function update(Request $request, int $id): mixed{
         $this->validate($request,self::MODEL::$rules);
         $model = self::MODEL::find($id);
         $model->update($request->all());
         return response()->json($model,200);
     }
 
-    public function delete(int $id): JsonResponse{
+    public function delete(int $id): mixed{
         self::MODEL::destroy($id);
         return response()->json(self::MODEL.' with id: '.$id.' removed',200);
     }
