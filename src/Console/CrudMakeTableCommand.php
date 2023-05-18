445c9177d35d8311c195ce271eb2c33b1c8d6d3b
vladmunj <mun.vladislav.a@gmail.com>
2023-05-18 10:37:49 +0600

    make crud table silent mode

--- src/Console/CrudMakeTableCommand.php
+++ src/Console/CrudMakeTableCommand.php
@@ -20,7 +20,7 @@ class CrudMakeTableCommand extends Command{
      *
      * @var string
      */
-    protected $signature = 'make:crud:table';
+    protected $signature = 'make:crud:table {--s}';
 
     /**
      * The console command description.
@@ -93,6 +93,7 @@ class CrudMakeTableCommand extends Command{
      * Asking user for set parameters
      */
     private function input(){
+        if($this->option('s')) return false;
         $this->info('Set table names, that be excluded from CRUD generation');
         $this->params['exceptions'] = $this->ask('Excluded table names');
     }
